9d98235332c285e9742383c607764962c9f6684d
gorshkovayulia <gorshkovayulia93@gmail.com>
2022-11-13 18:50:02 +0200

    Make SecurityConfig class as configuration

--- src/main/java/com/example/springsecurity/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.example.springsecurity;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
@@ -10,6 +11,7 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+@Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
