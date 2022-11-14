ce46780a2e3caadb53b41efc844a0819f1458f00
gorshkovayulia <gorshkovayulia93@gmail.com>
2022-11-14 15:41:37 +0200

    Override the AunthentificationProvider implementation

--- src/main/java/com/example/springsecurity/configs/ProjectConfig.java
+++ src/main/java/com/example/springsecurity/configs/ProjectConfig.java
@@ -1,14 +1,24 @@
 package com.example.springsecurity.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
- * Configuration class for authorization management
+ * Configuration class for user and password management
  */
 @Configuration
-public class WebAuthorizationConfig extends WebSecurityConfigurerAdapter {
+public class ProjectConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private CustomAuthenticationProvider authenticationProvider;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider);
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
