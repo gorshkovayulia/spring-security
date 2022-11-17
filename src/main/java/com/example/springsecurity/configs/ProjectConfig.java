21be52d046071a0b9f262aeb1f9b6e9e3046c46b
gorshkovayulia <gorshkovayulia93@gmail.com>
2022-11-17 20:56:59 +0200

    Start using of DelegatingPasswordEncoder

--- src/main/java/com/example/springsecurity/configs/ProjectConfig.java
+++ src/main/java/com/example/springsecurity/configs/ProjectConfig.java
@@ -3,10 +3,15 @@ package com.example.springsecurity.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Configuration class for user and password management
@@ -20,6 +25,11 @@ public class ProjectConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
+        Map<String, PasswordEncoder> encoders = new HashMap<>();
+        encoders.put("noop", NoOpPasswordEncoder.getInstance());
+        encoders.put("bcrypt", new BCryptPasswordEncoder());
+        encoders.put("scrypt", new SCryptPasswordEncoder());
+
+        return new DelegatingPasswordEncoder("bcrypt", encoders);
     }
 }
