25cfeddb331d0b604c8e9f38750c0ec67a9d82d4
gorshkovayulia <gorshkovayulia93@gmail.com>
2022-11-21 20:45:14 +0200

    Simplify app

--- src/main/java/com/example/springsecurity/controllers/HelloController.java
+++ src/main/java/com/example/springsecurity/controllers/HelloController.java
@@ -1,14 +1,11 @@
 package com.example.springsecurity.controllers;
 
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @GetMapping("/hello")
-    public String hello(Authentication a) {
-        return "Hello, " + a.getName() + "!";
+    public String hello() {
+        return "Hello!";
     }
 }
