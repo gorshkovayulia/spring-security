3f325d268037cc14c4b1880c82bc52136d826743
gorshkovayulia <gorshkovayulia93@gmail.com>
2022-11-21 21:25:34 +0200

    Revert "Customize the response for a failed authentication"
    This reverts commit 179046692520774365e0a6b1771a64698b49a4e7.

--- src/main/java/com/example/springsecurity/config/ProjectConfig.java
+++ src/main/java/com/example/springsecurity/config/ProjectConfig.java
@@ -1,6 +1,5 @@
 package com.example.springsecurity.config;
 
-import com.example.springsecurity.CustomEntryPoint;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -10,12 +9,7 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.httpBasic(c -> {
-            c.realmName("OTHER"); // changing the realm name
-            c.authenticationEntryPoint(new CustomEntryPoint());
-        });
-        http.authorizeRequests()
-                .anyRequest()
-                .authenticated();
+        http.httpBasic(c -> c.realmName("OTHER")); // changing the realm name
+        http.authorizeRequests().anyRequest().authenticated();
     }
 }
