8ae9e392010ffd9a447fbecf5ad53f5cc7f4d077
gorshkovayulia <gorshkovayulia93@gmail.com>
2022-11-25 21:13:42 +0200

    Use hasAnyAuthority() method for authorization

--- src/main/java/com/example/springsecurity/config/ProjectConfig.java
+++ src/main/java/com/example/springsecurity/config/ProjectConfig.java
@@ -36,6 +36,6 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.formLogin()
                 .defaultSuccessUrl("/main", true);
-        http.authorizeRequests().anyRequest().hasAuthority("WRITE");
+        http.authorizeRequests().anyRequest().hasAnyAuthority("WRITE", "READ");
     }
 }
