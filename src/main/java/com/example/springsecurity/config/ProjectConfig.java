a029af91f0170f46b4ebb3a170a63ffd36f20320
gorshkovayulia <gorshkovayulia93@gmail.com>
2022-11-27 14:41:01 +0200

    Simplify ProjectConfig class

--- src/main/java/com/example/springsecurity/config/ProjectConfig.java
+++ src/main/java/com/example/springsecurity/config/ProjectConfig.java
@@ -3,33 +3,15 @@ package com.example.springsecurity.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
-import org.springframework.security.oauth2.client.registration.ClientRegistration;
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
-import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.oauth2Login(c -> {
-            c.clientRegistrationRepository(clientRepository());
-        });
-        http.authorizeRequests().anyRequest().authenticated();
-    }
-
-    private ClientRegistration clientRegistration() {
-        return CommonOAuth2Provider.GITHUB
-                .getBuilder("github")
-                .clientId("b9660a23f74bee506af3")
-                .clientSecret("d217c3996734c94ade24f9c3e1201be508ebeb3a")
-                .build();
-    }
-
-    // adds a bean of type ClientRegistrationRepository to the Spring context
-    private ClientRegistrationRepository clientRepository() {
-        ClientRegistration cr = clientRegistration();
-        return new InMemoryClientRegistrationRepository(cr);
+        http.oauth2Login();
+        http.authorizeRequests()
+                .anyRequest()
+                .authenticated();
     }
 }
