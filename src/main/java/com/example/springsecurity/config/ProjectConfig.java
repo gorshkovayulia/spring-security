cc0b24cc401b76fcdbca3d7f956f69d78422c8fb
gorshkovayulia <gorshkovayulia93@gmail.com>
2022-11-27 14:23:25 +0200

    Implement ClientRegistration

--- src/main/java/com/example/springsecurity/config/ProjectConfig.java
+++ src/main/java/com/example/springsecurity/config/ProjectConfig.java
@@ -3,6 +3,8 @@ package com.example.springsecurity.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
@@ -11,5 +13,13 @@ public class ProjectConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.oauth2Login();
         http.authorizeRequests().anyRequest().authenticated();
+    }
+
+    private ClientRegistration clientRegistration() {
+        return CommonOAuth2Provider.GITHUB
+                .getBuilder("github")
+                .clientId("b9660a23f74bee506af3")
+                .clientSecret("d217c3996734c94ade24f9c3e1201be508ebeb3a")
+                .build();
     }
 }
