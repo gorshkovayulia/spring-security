8b4f1cdf7c15bf23a5152227e4a673790d6d97dd
gorshkovayulia <gorshkovayulia93@gmail.com>
2022-11-29 10:55:52 +0200

    Add filter at the location of another in the chain

--- src/main/java/com/example/springsecurity/config/ProjectConfig.java
+++ src/main/java/com/example/springsecurity/config/ProjectConfig.java
@@ -2,48 +2,23 @@ package com.example.springsecurity.config;
 
 import com.example.springsecurity.AuthenticationLoggingFilter;
 import com.example.springsecurity.RequestValidationFilter;
-import org.springframework.context.annotation.Bean;
+import com.example.springsecurity.StaticKeyAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-
-        UserDetails user1 = User.withUsername("john")
-                .password("12345")
-                .authorities("read")
-                .build();
-        UserDetails user2 = User.withUsername("jane")
-                .password("12345")
-                .authorities("read", "premium")
-                .build();
-
-        manager.createUser(user1);
-        manager.createUser(user2);
-
-        return manager;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return NoOpPasswordEncoder.getInstance();
-    }
+    @Autowired
+    private StaticKeyAuthenticationFilter filter;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.addFilterBefore(new RequestValidationFilter(), BasicAuthenticationFilter.class)
+                .addFilterAt(filter, BasicAuthenticationFilter.class)
                 .addFilterAfter(new AuthenticationLoggingFilter(), BasicAuthenticationFilter.class)
                 .authorizeRequests()
                 .anyRequest().permitAll();
