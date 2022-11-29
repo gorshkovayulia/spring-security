0687e7476c59fc4cf02954e64178a4075ff73410
gorshkovayulia <gorshkovayulia93@gmail.com>
2022-11-29 21:08:20 +0200

    Add custom filter

--- src/main/java/com/example/springsecurity/config/ProjectConfig.java
+++ src/main/java/com/example/springsecurity/config/ProjectConfig.java
@@ -1,25 +1,19 @@
 package com.example.springsecurity.config;
 
-import com.example.springsecurity.AuthenticationLoggingFilter;
-import com.example.springsecurity.RequestValidationFilter;
-import com.example.springsecurity.StaticKeyAuthenticationFilter;
+import com.example.springsecurity.CsrfTokenLogger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CsrfFilter;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private StaticKeyAuthenticationFilter filter;
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.addFilterBefore(new RequestValidationFilter(), BasicAuthenticationFilter.class)
-                .addFilterAt(filter, BasicAuthenticationFilter.class)
-                .addFilterAfter(new AuthenticationLoggingFilter(), BasicAuthenticationFilter.class)
+        http.addFilterAfter(new CsrfTokenLogger(), CsrfFilter.class)
                 .authorizeRequests()
                 .anyRequest().permitAll();
     }
--- src/main/java/com/example/springsecurity/controllers/HelloController.java
+++ src/main/java/com/example/springsecurity/controllers/HelloController.java
@@ -1,13 +1,19 @@
 package com.example.springsecurity.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public String hello() {
-        return "Hello!";
+    public String getHello() {
+        return "Get Hello!";
+    }
+
+    @PostMapping("/hello")
+    public String postHello() {
+        return "Post Hello!";
     }
 }
