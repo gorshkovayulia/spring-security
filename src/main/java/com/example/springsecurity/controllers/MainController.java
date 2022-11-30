33db4f6d51225efa5ccdc7ddcae62efa2a9ce73c
gorshkovayulia <gorshkovayulia93@gmail.com>
2022-11-30 20:57:06 +0200

    Add missed annotations

--- src/main/java/com/example/springsecurity/controllers/MainController.java
+++ src/main/java/com/example/springsecurity/controllers/MainController.java
@@ -1,7 +1,9 @@
 package com.example.springsecurity.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class MainController {
 
     @GetMapping("/main")
--- src/main/java/com/example/springsecurity/controllers/ProductController.java
+++ src/main/java/com/example/springsecurity/controllers/ProductController.java
@@ -1,10 +1,14 @@
 package com.example.springsecurity.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.logging.Logger;
 
+@Controller
+@RequestMapping("/product")
 public class ProductController {
 
     private Logger logger = Logger.getLogger(ProductController.class.getName());
