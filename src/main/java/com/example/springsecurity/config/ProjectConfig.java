a35a29863b0c0f1a332a79eb79f93d5ea4c27ea1
gorshkovayulia <gorshkovayulia93@gmail.com>
2022-12-03 12:34:21 +0200

    Apply CORS policies using @CrossOrigin annotation

--- src/main/java/com/example/springsecurity/config/ProjectConfig.java
+++ src/main/java/com/example/springsecurity/config/ProjectConfig.java
@@ -1,30 +1,15 @@
 package com.example.springsecurity.config;
 
-import com.example.springsecurity.repositories.CustomCsrfTokenRepository;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.csrf.CsrfTokenRepository;
 
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
-    @Bean
-    public CsrfTokenRepository customTokenRepository() {
-        return new CustomCsrfTokenRepository();
-    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        // where 'c' is a CsrfConfigurer
-        http.csrf(c -> {
-            // plug the new CsrfTokenRepository implementation into the CSRF protection mechanism
-            c.csrfTokenRepository(customTokenRepository());
-            // exclude this path from the CSRF protection mechanism
-            c.ignoringAntMatchers("/ciao");
-            c.ignoringAntMatchers("/h2-console/**");
-        });
+        http.csrf().disable();
 
         http.authorizeRequests()
                 .anyRequest().permitAll();
