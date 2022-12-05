1c921a1be9d8cfa65f6d3760985ba772ba1849d7
gorshkovayulia <gorshkovayulia93@gmail.com>
2022-12-05 18:03:02 +0200

    1. Add entity annotation to Otp class; 2. Add java docs and comments

--- src/main/java/com/example/springsecurity/config/ProjectConfig.java
+++ src/main/java/com/example/springsecurity/config/ProjectConfig.java
@@ -7,18 +7,20 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+/**
+ * The configuration class for the authentication server
+ */
 @Configuration
 public class ProjectConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return new BCryptPasswordEncoder(); // to hash the passwords stored in the DB
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable();
-        http.authorizeRequests()
-                .anyRequest().permitAll();
+        http.csrf().disable(); // so we can call all the endpoints of the app directly
+        http.authorizeRequests().anyRequest().permitAll(); // allows all the calls w/o authentication
     }
 }
--- src/main/java/com/example/springsecurity/utils/GenerateCodeUtil.java
+++ src/main/java/com/example/springsecurity/utils/GenerateCodeUtil.java
@@ -3,6 +3,9 @@ package com.example.springsecurity.utils;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 
+/**
+ * Generates a random number used as an OTP
+ */
 public final class GenerateCodeUtil {
 
     private GenerateCodeUtil() {}
@@ -10,12 +13,17 @@ public final class GenerateCodeUtil {
     public static String generateCode() {
         String code;
         try {
+            // create an instance of SecureRandom that generates a random int value
             SecureRandom random = SecureRandom.getInstanceStrong();
+
+            // generate a value between 0 and 8,999. We add 1,000 to each generated value.
+            // This way, we get values between 1,000 and 9,999 (4-digit random codes).
             int c = random.nextInt(9000) + 1000;
+
+            // convert the int to a String and returns it
             code = String.valueOf(c);
         } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(
-                    "Problem when generating the random code.");
+            throw new RuntimeException("Problem when generating the random code.");
         }
         return code;
     }
