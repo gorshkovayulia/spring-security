6092c04942cd54a59cd118b42abd56f274113fac
gorshkovayulia <gorshkovayulia93@gmail.com>
2022-12-06 20:43:00 +0200

    Define user management + use InMemoryClientDetailsService to configure a client

---
+++ src/main/java/com/example/springsecurity/config/AuthServerConfig.java
@@ -0,0 +1,41 @@
+package com.example.springsecurity.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.client.BaseClientDetails;
+import org.springframework.security.oauth2.provider.client.InMemoryClientDetailsService;
+
+import java.util.Collections;
+
+@Configuration
+@EnableAuthorizationServer // instruct Spring Boot to enable the configuration specific to the OAuth 2 authorization server
+public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
+        endpoints.authenticationManager(authenticationManager);
+    }
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        InMemoryClientDetailsService service = new InMemoryClientDetailsService();
+
+        BaseClientDetails cd = new BaseClientDetails();
+        cd.setClientId("client");
+        cd.setClientSecret("secret");
+        cd.setScope(Collections.singletonList("read"));
+        cd.setAuthorizedGrantTypes(Collections.singletonList("password"));
+
+        service.setClientDetailsStore(Collections.singletonMap("client", cd));
+
+        clients.withClientDetails(service);
+    }
+}
