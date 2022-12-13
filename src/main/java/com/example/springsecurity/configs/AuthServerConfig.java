567d831c67d60e63b0afefbae138f455db3b39cd
gorshkovayulia <gorshkovayulia93@gmail.com>
2022-12-13 22:02:33 +0200

    Implement the authorization server to issue JWTs

--- src/main/java/com/example/springsecurity/configs/AuthServerConfig.java
+++ src/main/java/com/example/springsecurity/configs/AuthServerConfig.java
@@ -1,6 +1,7 @@
-package com.example.springsecurity.configs;
+package com.example.springsecurity.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -8,21 +9,19 @@ import org.springframework.security.oauth2.config.annotation.configurers.ClientD
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
-
-import javax.sql.DataSource;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer // instruct Spring Boot to enable the configuration specific to the OAuth 2 authorization server
 public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
-    @Autowired
-    private AuthenticationManager authenticationManager;
+    @Value("${jwt.key}") // Gets the value of the symmetric key from the application.yml file
+    private String jwtKey;
 
     @Autowired
-    private DataSource dataSource;
+    private AuthenticationManager authenticationManager;
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -35,19 +34,24 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
-        endpoints
-                .authenticationManager(authenticationManager)
-                .tokenStore(tokenStore());
+        endpoints.
+                authenticationManager(authenticationManager).
+                // Configures the token store and the access token converter objects
+                tokenStore(tokenStore())
+                .accessTokenConverter(jwtAccessTokenConverter());;
     }
 
     @Bean
     public TokenStore tokenStore() {
-        return new JdbcTokenStore(dataSource);
+        // Creates a token store with an access token converter associated to it
+        return new JwtTokenStore(jwtAccessTokenConverter());
     }
 
-    // To allow authenticated requests to call the /oauth/check_token endpoint
-    public void configure(AuthorizationServerSecurityConfigurer security) {
-        // Specify the condition for which we can call the check_token endpoint
-        security.checkTokenAccess("isAuthenticated()");
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        // Sets the value of the symmetric key for the access token converter object
+        converter.setSigningKey(jwtKey);
+        return converter;
     }
 }
--- src/main/java/com/example/springsecurity/configs/WebSecurityConfig.java
+++ src/main/java/com/example/springsecurity/configs/WebSecurityConfig.java
@@ -3,7 +3,6 @@ package com.example.springsecurity.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -40,10 +39,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     public AuthenticationManager authenticationManagerBean() throws Exception {
         return super.authenticationManagerBean();
-    }
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin();
     }
 }
