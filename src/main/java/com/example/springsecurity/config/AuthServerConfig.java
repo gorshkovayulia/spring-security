c71e818f020dd8a3e1cda1327fe430369dcc4ef1
gorshkovayulia <gorshkovayulia93@gmail.com>
2022-12-13 22:02:33 +0200

    Implement the authorization server to issue JWTs

--- src/main/java/com/example/springsecurity/config/AuthServerConfig.java
+++ src/main/java/com/example/springsecurity/config/AuthServerConfig.java
@@ -1,18 +1,24 @@
 package com.example.springsecurity.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
-
-import java.util.Collections;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
 @EnableAuthorizationServer // instruct Spring Boot to enable the configuration specific to the OAuth 2 authorization server
 public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Value("${jwt.key}") // Gets the value of the symmetric key from the application.yml file
+    private String jwtKey;
 
     @Autowired
     private AuthenticationManager authenticationManager;
@@ -28,6 +34,24 @@ public class AuthServerConfig extends AuthorizationServerConfigurerAdapter {
 
     @Override
     public void configure(AuthorizationServerEndpointsConfigurer endpoints) {
-        endpoints.authenticationManager(authenticationManager);
+        endpoints.
+                authenticationManager(authenticationManager).
+                // Configures the token store and the access token converter objects
+                tokenStore(tokenStore())
+                .accessTokenConverter(jwtAccessTokenConverter());;
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        // Creates a token store with an access token converter associated to it
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        // Sets the value of the symmetric key for the access token converter object
+        converter.setSigningKey(jwtKey);
+        return converter;
     }
 }
