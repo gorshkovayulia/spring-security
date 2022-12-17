56c08979ab168e4867c4534973ffba143903c142
gorshkovayulia <gorshkovayulia93@gmail.com>
2022-12-17 20:22:28 +0200

    Implement a resource server that uses JWT

--- src/main/java/com/example/springsecurity/configs/ResourceServerConfig.java
+++ src/main/java/com/example/springsecurity/configs/ResourceServerConfig.java
@@ -1,32 +1,36 @@
-package com.example.springsecurity.configs;
+package com.example.springsecurity.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
-
-import javax.sql.DataSource;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 
 @Configuration
-@EnableResourceServer // instruct Spring Boot to enable the configuration specific to the OAuth 2 resource server
+@EnableResourceServer
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
-    @Autowired
-    private DataSource dataSource;
+    @Value("${jwt.key}")
+    private String jwtKey;
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) {
-        // Configures the token store
         resources.tokenStore(tokenStore());
     }
 
-    // Creates a JdbcTokenStore based on the injected data source
     @Bean
     public TokenStore tokenStore() {
-        return new JdbcTokenStore(dataSource);
+        return new JwtTokenStore(jwtAccessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter jwtAccessTokenConverter() {
+        JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(jwtKey);
+        return converter;
     }
 }
