b11a37ccdf784c742062f3cfd34b0471e1438cd1
gorshkovayulia <gorshkovayulia93@gmail.com>
2022-12-25 16:22:49 +0200

    use the values of the method parameters

--- src/main/java/com/example/springsecurity/controllers/HelloController.java
+++ src/main/java/com/example/springsecurity/controllers/HelloController.java
@@ -3,7 +3,10 @@ package com.example.springsecurity.controllers;
 import com.example.springsecurity.services.NameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HelloController {
@@ -11,8 +14,10 @@ public class HelloController {
     @Autowired
     private NameService nameService;
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "Hello, " + nameService.getName();
+    @GetMapping("/secret/names/{name}") // Defines an endpoint that takes a value from a path variable
+    public List<String> names(@PathVariable String name) {
+
+        // Calls the protected method to obtain the secret names of the users
+        return nameService.getSecretNames(name);
     }
 }
--- src/main/java/com/example/springsecurity/services/NameService.java
+++ src/main/java/com/example/springsecurity/services/NameService.java
@@ -3,11 +3,18 @@ package com.example.springsecurity.services;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
+import java.util.*;
+
 @Service
 public class NameService {
 
-    @PreAuthorize("hasAuthority('write')") // Defines the authorization rule.
-    public String getName() {
-        return "Fantastico";
+    private final HashMap<String, List<String>> secretNames = new HashMap<String, List<String>>() {{
+        put("natalie", Arrays.asList("Energico", "Perfecto"));
+        put("emma", Collections.singletonList("Fantastico"));
+    }};
+
+    @PreAuthorize("#name == authentication.principal.username") // Uses #name to represent the value of the method parameters in the authorization expression
+    public List<String> getSecretNames(String name) {
+        return secretNames.get(name);
     }
 }
